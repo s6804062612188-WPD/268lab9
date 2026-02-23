6df7015fc44a51d33349a464acac0974a941ade4
Wongsathon Panthudech <s6804062612188@email.kmutnb.ac.th>
2026-02-23 22:25:57 +0700

    logging in
    but it's kinda chaotic

---
+++ lab93no3.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define size 64
+
+int checkLogin(char *login, char *passwd, char *logindb, char *passdb); 
+
+int stringCmp(char *str1, char *str2) {
+	int i=0;
+	while (str1[i]!='\0' && str2[i]!='\0') {
+		//printf("%c %c\n", str1[i], str2[i] );
+		if (str1[i]==str2[i]) i=i+1;
+		else break;
+	}
+	if (str1[i]=='\0' && str2[i]=='\0') return 1;
+	else return 0;
+}
+
+int main() {
+	char login[5][size]={"user1","user2","user3","user4","user5"};
+	char password[5][size]={"pass1","pass2","pass3","pass4","pass5"};
+	
+	char user[size], pass[size];
+	printf("Enter your username:\n");	scanf("%s", user);
+	printf("Enter your password:\n");	scanf("%s", pass);
+	
+	checkLogin(user, pass, login[0], password[0]);
+}
+
+int checkLogin(char *login, char *passwd, char *logindb, char *passdb) {
+	int id=-1;
+	for (int i=0; i<5; i=i+1) {
+		if ( stringCmp( login, logindb+i*size ) ) {
+			id=i; break;
+		}
+	}
+	
+	if (id>=0) {
+		if ( stringCmp( passwd, passdb+id*size) ) {
+			printf("Welcome\n"); return 0;
+		}
+	}
+	printf("Incorrect login or password\n");
+	return 0;
+}
