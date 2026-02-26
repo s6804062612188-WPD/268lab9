7f7d2e6cf41d43f38ad8ea3a4cc780026289a939
Wongsathon Panthudech <s6804062612188@email.kmutnb.ac.th>
2026-02-27 00:58:29 +0700

    logging in
    manual stringCmp is optimized

--- lab93no3.cpp
+++ lab93no3.cpp
@@ -5,13 +5,10 @@ int checkLogin(char *login, char *passwd, char *logindb, char *passdb);
 
 int stringCmp(char *str1, char *str2) {
 	int i=0;
-	while (str1[i]!='\0' && str2[i]!='\0') {
-		//printf("%c %c\n", str1[i], str2[i] );
-		if (str1[i]==str2[i]) i=i+1;
-		else break;
-	}
-	if (str1[i]=='\0' && str2[i]=='\0') return 1;
-	else return 0;
+	while (str1[i]==str2[i]) {
+		if (str1[i]=='\0') return 1;
+		i=i+1;
+	} return 0;
 }
 
 int main() {
